3346e31fce9a85a584a0bfe8b83f2e62dbd35f05
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2020-11-23 09:45:21 -0300

    abance v.desktop

--- js/configuracion-usuario.js
+++ js/configuracion-usuario.js
@@ -1,21 +1,7 @@
-let container = document.querySelector(".container-partial");
-let links = document.querySelectorAll("a");
-links.forEach( elem => {
-    elem.onclick = (e) => {
-        e.preventDefault();
-        urlData = elem.getAttribute("href");
-        fetch(urlData)
-        .then(function(r){
-        r.text().then(function(html) {
-            container.innerHTML = html;
-        })
-        })
-    }
+let container = document.querySelector(".container-secundario");
+fetch("../menu-configuracion.html").then(function(r){
+    r.text().then(function(html) {
+        container.innerHTML = html;
+    })
 })
 
-// fetch('../configuracion-perfil.html')
-//  .then(function(r){
-//  r.text().then(function(html) {
-//     container.innerHTML = html;
-//  })
-//  })
