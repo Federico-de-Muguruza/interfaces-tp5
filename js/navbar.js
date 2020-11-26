fa9251881ec17048b5c1d49a6cb554da7e917075
Muguckz <fedemuguruza@gmail.com>
2020-11-26 00:37:06 -0300

    Se agrearon los dos navbar sin sesión y con sesión a todos los templates

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+let dropdown = document.querySelector("#dropdown");
+
+dropdown.addEventListener("click", mostrarMenuOculto);
+
+function mostrarMenuOculto() {
+    let menu_oculto = document.querySelector("#menu-oculto");
+
+    console.log(menu_oculto)
+    menu_oculto.classList.toggle("d-none");
+}
