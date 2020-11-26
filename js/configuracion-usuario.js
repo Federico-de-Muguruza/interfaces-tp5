bde22c6880767c1cf3b36fe8c359e9425e1a20b7
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2020-11-26 11:25:10 -0300

    agregado de elementos toggle configuracion-perfil

--- js/configuracion-usuario.js
+++ js/configuracion-usuario.js
@@ -5,3 +5,19 @@ fetch("../menu-configuracion.html").then(function(r){
     })
 })
 
+let botonesAbrir = document.querySelectorAll(".btn-toggle-perfil");
+if(botonesAbrir) {
+    botonesAbrir.forEach( elem => {
+        console.log(elem);
+        elem.addEventListener("click", function (e)  {
+            // elem.classList.add("inexistente");
+            e.preventDefault();
+            let idDiv = elem.getAttribute("for");
+            let elemModif = document.querySelector(`#${idDiv} form`);
+            elemModif.classList.toggle("inexistente");
+            let btnEdit = document.querySelector(`#${idDiv} .btn-abrir-perfil`);
+            btnEdit.classList.toggle("inexistente");
+        }
+    )
+})
+}
