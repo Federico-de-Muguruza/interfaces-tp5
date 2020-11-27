d40f4bfdbda3f27f901829b14c19c82536d233c3
Muguckz <fedemuguruza@gmail.com>
2020-11-26 23:42:27 -0300

    Se puso el icono de play al hover de cada tema

--- js/navbar.js
+++ js/navbar.js
@@ -5,6 +5,6 @@ dropdown.addEventListener("click", mostrarMenuOculto);
 function mostrarMenuOculto() {
     let menu_oculto = document.querySelector("#menu-oculto");
 
-    console.log(menu_oculto)
+    console.log(menu_oculto);
     menu_oculto.classList.toggle("d-none");
 }
