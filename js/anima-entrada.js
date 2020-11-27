cb6a34e348cdba561436c617686bc1ef634d8bf2
GTCP <55481093+GTCP@users.noreply.github.com>
2020-11-27 11:15:36 -0300

    animacion de entrada de letras home y busquedas

---
+++ js/anima-entrada.js
@@ -0,0 +1,5 @@
+ let ElijahWood=document.querySelector('.ElijahWood');
+    let OrlandoBloom=document.querySelector('.OrlandoBloom');
+    let ViggoMortensen=document.querySelector('.ViggoMortensen');
+    let JohnRhysDavies=document.querySelector('.JohnRhys-Davies');
+    let IanMcKellen=document.querySelector('.IanMcKellen');
