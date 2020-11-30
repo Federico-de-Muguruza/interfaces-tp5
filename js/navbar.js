60039e5ef4dc4b82724032efc108562fd925a829
Muguckz <fedemuguruza@gmail.com>
2020-11-30 01:23:20 -0300

    Se arregló el dropdown del navbar-fuera (Mobile) y se cambiaron colores a los navbar. Para arreglar: navbar-dentro (Mobile y Desktop, css y dropdown)

--- js/navbar.js
+++ js/navbar.js
@@ -1,34 +1,48 @@
-// document.querySelector("#nav-dropdown-fuera").addEventListener("click", function () {
-//     let menu_oculto = document.querySelector("#nav-menu-oculto-fuera");
-//     menu_oculto.classList.toggle("menu-oculto");
-//     menu_oculto.classList.toggle("existente");
-// })
+"use strict"
 
-// document.querySelector(".nav-fuera").addEventListener("click", function () {
-//     window.location = "../html/index.html";
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    let nav_dropdown_fuera = document.querySelector("#nav-dropdown-fuera")
 
-let notificaciones = document.querySelector("#btn-nav-menu-oculto-dentro-notificaciones");
-let perfil = document.querySelector("#btn-nav-menu-oculto-dentro-perfil");
-let menu_oculto_perfil = document.querySelector("#nav-menu-oculto-dentro-perfil");
-let menu_oculto_notificaciones = document.querySelector("#nav-menu-oculto-dentro-notificaciones");
+    if (nav_dropdown_fuera) {
+        nav_dropdown_fuera.addEventListener("click", function () {
+            let menu_oculto = document.querySelector("#nav-menu-oculto-fuera");
+            menu_oculto.classList.toggle("menu-oculto");
+            menu_oculto.classList.toggle("existente");
+        })
+    }
 
-notificaciones.addEventListener("click", function () {
-    menu_oculto_notificaciones.classList.toggle("menu-oculto");
-    menu_oculto_notificaciones.classList.toggle("existente");
+    // document.querySelector(".nav-fuera").addEventListener("click", function () {
+    //     window.location = "../html/index.html";
+    // })
+    
+    let notificaciones = document.querySelector("#btn-nav-menu-oculto-dentro-notificaciones");
+    let perfil = document.querySelector("#btn-nav-menu-oculto-dentro-perfil");
+    let menu_oculto_perfil = document.querySelector("#nav-menu-oculto-dentro-perfil");
+    let menu_oculto_notificaciones = document.querySelector("#nav-menu-oculto-dentro-notificaciones");
+    
+    if (notificaciones && perfil) {
+        notificaciones.addEventListener("click", function () {
+            menu_oculto_notificaciones.classList.toggle("menu-oculto");
+            menu_oculto_notificaciones.classList.toggle("existente");
+        
+                menu_oculto_perfil.classList.remove("existente");
+                menu_oculto_perfil.classList.add("menu-oculto");
+        })
 
-        menu_oculto_perfil.classList.remove("existente");
-        menu_oculto_perfil.classList.add("menu-oculto");
+        perfil.addEventListener("click", function () {
+            menu_oculto_perfil.classList.toggle("menu-oculto");
+            menu_oculto_perfil.classList.toggle("existente");
+        
+                menu_oculto_notificaciones.classList.add("menu-oculto");
+                menu_oculto_notificaciones.classList.remove("existente");
+        })
+
+        document.querySelector(".nav-home").addEventListener("click", function () {
+            window.location = "../html/inicio-registrado.html";
+        })
+    }
+    
+    
 })
 
-perfil.addEventListener("click", function () {
-    menu_oculto_perfil.classList.toggle("menu-oculto");
-    menu_oculto_perfil.classList.toggle("existente");
 
-        menu_oculto_notificaciones.classList.add("menu-oculto");
-        menu_oculto_notificaciones.classList.remove("existente");
-})
-
-document.querySelector(".nav-home").addEventListener("click", function () {
-    window.location = "../html/inicio-registrado.html";
-})
